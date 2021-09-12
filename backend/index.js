8f0d890dc8625da701b1bbf0b3155fda6abfcf0d
Mohit Maroliya <mmaroliya@gmail.com>
2021-09-12 08:31:00 +0530

    Storing the data in Mongo Atlas using post request

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+//to use req body 
+app.use(express.json())
+
 //Available routes
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
