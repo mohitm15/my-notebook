19fce8cfe4203f7355c8552fd67c51d4c7c44a29
Mohit Maroliya <mmaroliya@gmail.com>
2021-09-30 20:23:46 +0530

    Redesigning the UI of LoginPage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,56 +1,107 @@
-import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import imgpath from "../assets/notepic.jpg";
 
 const Login = (props) => {
+  let history = useHistory();
 
-    let history = useHistory();
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
 
-    const [credentials, setCredentials] = useState({email:"", password:""});
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+    //input mei value typed ho sake,jaise jaise value change ho vese-vese note me set ho jaye
+  };
 
-    const onChange = (e) => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value });
-        //input mei value typed ho sake,jaise jaise value change ho vese-vese note me set ho jaye
-      };
-
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        const response = await fetch('http://localhost:5000/api/auth/login', {
-            method: 'POST', 
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({email: credentials.email, password: credentials.password})
-          });
-          const json = await response.json();
-          if(json.success === true) {
-            //storing the authtoken
-            localStorage.setItem('token',json.authToken);
-            props.showAlert("User logged in successfully", "info");
-            history.push("/");
-          } 
-          else {
-              props.showAlert("invalid Credentials", "danger")
-          }
-          console.log(json);
+  const goToSignup = () => {
+    history.push("/signup");
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+    });
+    const json = await response.json();
+    if (json.success === true) {
+      //storing the authtoken
+      localStorage.setItem("token", json.authToken);
+      props.showAlert("User logged in successfully", "info");
+      history.push("/");
+    } else {
+      props.showAlert("invalid Credentials", "danger");
     }
+    console.log(json);
+  };
 
-    return (
-        <div className='mt-3'>
-            <h2 className="my-3"> Login to continue to use my-notebook</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" name="email" value={credentials.email} aria-describedby="emailHelp"  onChange={onChange}/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={onChange}/>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+  return (
+    <div className="container" id="manku">
+      <div id="picturebody">
+        <img src={imgpath} alt="note-pic" width="100%" />
+      </div>
+      <div id="loginbody">
+        <div className="mt-3">
+          <h2 className="my-3 display-5"> Login to continue</h2>
+          <form onSubmit={handleSubmit} className="login-form">
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={credentials.email}
+                aria-describedby="emailHelp"
+                onChange={onChange}
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={credentials.password}
+                onChange={onChange}
+              />
+            </div>
+            <div className="d-grid gap-2 my-5 col-6 mx-auto">
+              <button type="submit" className="btn btn-success ">
+                Log In
+              </button>
+            </div>
+            <hr />
+            <div className="mb-3 text-center">
+              <div id="emailHelp" className="form-text center my-3">
+                Didn't have an account ?
+              </div>
+              <div className="d-grid gap-2 my-3 col-6 mx-auto">
+                <button onClick={goToSignup} className="btn btn-success ">
+                  SignUp Here !
+                </button>
+              </div>
+            </div>
+          </form>
+          <div className="text-center my-5" id="bottom-text">
+            mynotebook
         </div>
-    )
-}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
