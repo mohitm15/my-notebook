dc4243efb447626e2ba55006e90fb0160af43121
Mohit Maroliya <mmaroliya@gmail.com>
2021-10-11 05:23:51 +0530

    Adding the delete modal

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import noteContext from "../context/notes/noteContext";
 import themeContext from "../context/themes/themeContext";
 
@@ -6,16 +6,24 @@ const Noteitem = (props) => {
   const contextForNotes = useContext(noteContext);
   const { deleteNote } = contextForNotes;
   const { note, updateNote } = props;
-
+  // updatenote is used here by edit-icon
   const contextForThemes = useContext(themeContext);
   const { theme } = contextForThemes;
 
-  // updatenote is used here by edit-icon
+  const ref = useRef(null);
+  const refClose = useRef(null);
+
 
   const handleDelete = () => {
+    ref.current.click();
+    //it calls the handle click of the delete button in modal which is used when we click icon of delete
+  };
+
+  const handleClick = () => {
     deleteNote(note._id);
     props.showAlert("Note Deleted successfully!", "danger");
-  };
+    refClose.current.click();
+  }
 
   let cardStyle = {
     backgroundColor: "light",
@@ -28,8 +36,12 @@ const Noteitem = (props) => {
   let tagBgColor = {
     backgroundColor: "#b2dbf7",
     color: "#39739d",
-    fontWeight:'500',
+    fontWeight: '500',
   };
+  let modalStyle = {
+    backgroundColor:'white',
+    color:'black',
+  }
 
   if (theme.light) {
     cardStyle = {
@@ -43,7 +55,11 @@ const Noteitem = (props) => {
     tagBgColor = {
       backgroundColor: "#b2dbf7",
       color: "#39739d",
-      fontWeight:'500',
+      fontWeight: '500',
+    };
+    modalStyle = {
+      backgroundColor:'white',
+      color:'black',
     };
 
   } else if (theme.dark) {
@@ -59,8 +75,12 @@ const Noteitem = (props) => {
       backgroundColor: "#435460",
       color: "#cde1ee",
       borderColor: "#00000000",
-      fontWeight:'500', 
+      fontWeight: '500',
     };
+    modalStyle = {
+      backgroundColor: "#2d2d2d",
+      color: "whitesmoke",
+    }
 
   } else if (theme.anotherDark) {
     cardStyle = {
@@ -73,9 +93,13 @@ const Noteitem = (props) => {
     };
     tagBgColor = {
       backgroundColor: "#3d3d3d",
-      color:'#00c548',
-      fontWeight:'500',
+      color: '#00c548',
+      fontWeight: '500',
     };
+    modalStyle = {
+      backgroundColor: "black",
+      color: "#00c548",
+    }
   } else if (theme.darkTeal) {
     cardStyle = {
       backgroundColor: "#045b62",
@@ -89,14 +113,39 @@ const Noteitem = (props) => {
       backgroundColor: "#42959e",
       color: "#cde1ee",
       borderColor: "#00000000",
-      fontWeight:'500', 
+      fontWeight: '500',
     };
+    modalStyle = {
+      backgroundColor:'#045b62',
+      color:'white',
+    }
 
   }
 
 
   return (
     <div className="col-md-3">
+
+      <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#deleteModal">
+        Launch demo modal
+      </button>
+
+      <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content" style={modalStyle}>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div className="modal-body text-center">
+              <h4>Delete Note ?</h4>
+            </div>
+            <div className="modal-footer">
+              <button ref={refClose} type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+              <button type="button" className="btn btn-danger" onClick={handleClick}>Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
       <div className={`card text  mb-3 my-3`} id="notecard" style={cardStyle}>
         <div className="card-header " style={cardHeader}>
           NOTE
@@ -120,14 +169,14 @@ const Noteitem = (props) => {
         <hr />
 
         <div className="pb-3 mx-2">
-          {note.tag.split(';').map((item,key)=>{
+          {note.tag.split(';').map((item, key) => {
             return (
               <span key={key} className="badge  mx-1 px-2 py-1" style={tagBgColor}>{item}</span>
             )
           })}
         </div>
-        
-        
+
+
       </div>
     </div>
   );
