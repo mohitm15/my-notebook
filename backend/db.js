251919fbf492736bd9e8d71c303c991a3926fc23
Mohit Maroliya <mmaroliya@gmail.com>
2022-01-26 16:20:30 +0530

    Adding the Mongo Atlas URL

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-
+//const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = "mongodb+srv://mohit_maroliya:Zker42FNU.buD_E@cluster0.n03hm.mongodb.net/iNotebook?retryWrites=true&w=majority"
 
 const connectToMongo = async() => {
     mongoose.connect(mongoURI, {useNewUrlParser: true}, ()=> {
