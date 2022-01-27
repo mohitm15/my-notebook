9ad079e8df380a2b5e05547c9ea9917c1a2108d5
Mohit Maroliya <mmaroliya@gmail.com>
2022-01-27 16:24:16 +0530

    Removing the race condition-6

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 //const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-const mongoURI =
-    "mongodb+srv://mohit_maroliya:gw$@yG9W++%j@cluster0.n03hm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://mohit_maroliya:gw$@yG9W++%j@cluster0.n03hm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const connectToMongo = mongoose.connect(
-    mongoURI,
-    { useNewUrlParser: true },
-    () => {
-        console.log("Connected to Mongo Successfully!");
+const connectToMongo = async() => {
+    try {
+          mongoose.connect(mongoURI, {useNewUrlParser: true}, ()=> {
+            console.log("Connected to Mongo Successfully!");
+        })
     }
-);
+    catch (e) {
+        console.error(e);
+    }
+}
 
 module.exports = connectToMongo;
